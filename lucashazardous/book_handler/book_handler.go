8bee7d23448d93015794ee6200e955a51391f6f8
Adam W <developeradamw@gmail.com>
2022-04-02 17:13:19 +0200

    delete book, admin panel and handler

--- lucashazardous/book_handler/book_handler.go
+++ lucashazardous/book_handler/book_handler.go
@@ -24,80 +24,7 @@ type bookHandler struct {
 	panel   adminPanel
 }
 
-func (b *bookHandler) Books(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		b.Get(w, r)
-		return
-	case "POST":
-		b.Post(w, r)
-		return
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("not allowed"))
-		return
-	}
-}
-
-func (b *bookHandler) Post(w http.ResponseWriter, r *http.Request) {
-	user, password, ok := r.BasicAuth()
-	if !ok || user != "admin" || password != b.panel.password {
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("401"))
-		return
-	}
-
-	bodyBytes, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
-
-	ct := r.Header.Get("content-type")
-	if ct != "application/json" {
-		w.WriteHeader(http.StatusUnsupportedMediaType)
-		w.Write([]byte("need content application/json"))
-		return
-	}
-
-	var book Book
-	err = json.Unmarshal(bodyBytes, &book)
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-	}
-
-	book.Id = fmt.Sprintf("%d", time.Now().UnixNano())
-
-	b.Lock()
-	b.library[book.Id] = book
-	b.Unlock()
-}
-
-func (b *bookHandler) Get(w http.ResponseWriter, r *http.Request) {
-
-	books := make([]Book, len(b.library))
-
-	b.Lock()
-	i := 0
-	for _, book := range b.library {
-		books[i] = book
-		i++
-	}
-	b.Unlock()
-
-	jsonBytes, err := json.Marshal(books)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-}
-
-func NewBookHandlers(panel *adminPanel) *bookHandler {
+func NewLibraryHandler(panel *adminPanel) *bookHandler {
 	return &bookHandler{
 		library: map[string]Book{
 			"1": {
@@ -112,10 +39,108 @@ func NewBookHandlers(panel *adminPanel) *bookHandler {
 	}
 }
 
-func (b *bookHandler) GetBook(w http.ResponseWriter, r *http.Request) {
+func (b *bookHandler) BooksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		b.getBooks(w, r)
+		return
+	case "POST":
+		b.createBook(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (b *bookHandler) SpecificBookHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "DELETE":
+		b.deleteSpecificBookById(w, r)
+	case "GET":
+		b.getSpecificBookById(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (b *bookHandler) createBook(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if username != "admin" || password != b.panel.password || !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte("need content application/json"))
+		return
+	}
+
+	var book Book
+	err = json.Unmarshal(bodyBytes, &book)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	book.Id = fmt.Sprintf("%d", time.Now().UnixNano())
+
+	b.Lock()
+	b.library[book.Id] = book
+	b.Unlock()
+}
+
+func (b *bookHandler) getBooks(w http.ResponseWriter, r *http.Request) {
+
+	books := make([]Book, len(b.library))
+
+	b.Lock()
+	i := 0
+	for _, book := range b.library {
+		books[i] = book
+		i++
+	}
+	b.Unlock()
+
+	jsonBytes, err := json.Marshal(books)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (b *bookHandler) deleteSpecificBookById(w http.ResponseWriter, r *http.Request) {
 	urlParts := strings.Split(r.URL.String(), "/")
 	if len(urlParts) != 3 {
 		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	b.Lock()
+	delete(b.library, urlParts[2])
+	b.Unlock()
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (b *bookHandler) getSpecificBookById(w http.ResponseWriter, r *http.Request) {
+	urlParts := strings.Split(r.URL.String(), "/")
+	if len(urlParts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
 	b.Lock()
@@ -130,7 +155,7 @@ func (b *bookHandler) GetBook(w http.ResponseWriter, r *http.Request) {
 	jsonBytes, err := json.Marshal(book)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		return
 	}
 
 	w.Header().Add("content-type", "application/json")
@@ -151,17 +176,28 @@ func NewAdminPanel() *adminPanel {
 }
 
 func (a adminPanel) AdminHandler(w http.ResponseWriter, r *http.Request) {
-	user, password, ok := r.BasicAuth()
-	if !ok || user != "admin" || password != a.password {
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("401"))
+	switch r.Method {
+	case "GET":
+		a.getAdminWebsite(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+}
+
+func (a adminPanel) getAdminWebsite(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if username != "admin" || password != a.password || !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	adminPage, err := ioutil.ReadFile("./admin_static/index.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("unable to read admin panel"))
 		return
 	}
+
 	w.Write(adminPage)
 }
--- lucashazardous/server.go
+++ lucashazardous/server.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	adminPanel := book_handler.NewAdminPanel()
-	bookHandler := book_handler.NewBookHandlers(adminPanel)
+	bookHandler := book_handler.NewLibraryHandler(adminPanel)
 	fileServer := http.FileServer(http.Dir("./static"))
 
 	go http.Handle("/", http.StripPrefix("/", fileServer))
 
-	go http.HandleFunc("/books", bookHandler.Books)
-	go http.HandleFunc("/books/", bookHandler.GetBook)
+	go http.HandleFunc("/books", bookHandler.BooksHandler)
+	go http.HandleFunc("/books/", bookHandler.SpecificBookHandler)
 
 	go http.HandleFunc("/admin", adminPanel.AdminHandler)
 
